6e4f8790c12fc345489a660d829f1b14a8a83dd5
Simon Stevenson <redwyre@users.noreply.github.com>
2015-04-01 21:50:29 +1000

    Got single solution building .net 2.0, .net 3.5, and .net 4.0 client versions of the assembly

--- SubstrateCS/Substrate.Entities/RegisterTileEntities.cs
+++ SubstrateCS/Substrate.Entities/RegisterTileEntities.cs
@@ -1,9 +1,6 @@
-﻿using Substrate.TileEntities;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Substrate.TileEntities;
 
 namespace Substrate.Entities
 {
