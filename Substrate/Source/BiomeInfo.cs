b4fd330155485e3d8c92e4d46bd27711018da3ef
Simon Stevenson <redwyre@outlook.com>
2015-04-03 16:09:55 +1000

    Updated biomes

--- Substrate/Source/BiomeInfo.cs
+++ Substrate/Source/BiomeInfo.cs
@@ -6,6 +6,8 @@ namespace Substrate
 {
     public class BiomeType
     {
+        public const int Uncalculated = -1;
+
         public const int Ocean = 0;
         public const int Plains = 1;
         public const int Desert = 2;
@@ -29,6 +31,44 @@ namespace Substrate
         public const int ExtremeHillsEdge = 20;
         public const int Jungle = 21;
         public const int JungleHills = 22;
+        public const int JungleEdge = 23;
+        public const int DeepOcean = 24;
+        public const int StoneBeach = 25;
+        public const int ColdBeach = 26;
+        public const int BirchForest = 27;
+        public const int BirchForestHills = 28;
+        public const int RoofedForest = 29;        
+        public const int ColdTaiga = 30;
+        public const int ColdTaigaHills = 31;
+        public const int MegaTaiga = 32;
+        public const int MegaTaigaHills = 33;
+        public const int ExtremeHillsPlus = 34; // Extreme Hills+
+        public const int Savanna = 35;
+        public const int SavannaPlateau = 36;
+        public const int Mesa = 37;
+        public const int MesaPlateauF = 38;
+        public const int MesaPlateau = 39;        
+        public const int SunflowerPlains = 129;
+        public const int DesertM = 130;
+        public const int ExtremeHillsM = 131;
+        public const int FlowerForest = 132;
+        public const int TaigaM = 133;
+        public const int SwamplandM = 134;        
+        public const int IcePlainsSpikes = 140;
+        public const int JungleM = 149;        
+        public const int JungleEdgeM = 151;
+        public const int BirchForestM = 155;
+        public const int BirchForestHillsM = 156;
+        public const int RoofedForestM = 157;
+        public const int ColdTaigaM = 158;        
+        public const int MegaSpruceTaiga = 160;
+        public const int RedwoodTaigaHillsM = 161;
+        public const int ExtremeHillsPlusM = 162; // Extreme Hills+ M
+        public const int SavannaM = 163;
+        public const int SavannaPlateauM = 164;
+        public const int MesaBryce = 165; // Mesa (Bryce)
+        public const int MesaPlateauFM = 166;
+        public const int MesaPlateauM = 167;
 
         public const int Default = 255;
     }
