918f8f38a42ba071f7908df7c5c23f965b7716a6
Shmueli Englard <shmueli.yosef@englard.net>
2018-07-19 13:03:25 -0400

    Fixed item schema

--- Substrate/Source/Item.cs
+++ Substrate/Source/Item.cs
@@ -59,7 +59,7 @@ namespace Substrate
         private static readonly SchemaNodeCompound _schema = new SchemaNodeCompound("")
         {
             new SchemaNodeString("id"),
-            new SchemaNodeScalar("Damage", TagType.TAG_SHORT),
+            new SchemaNodeScalar("Damage", TagType.TAG_SHORT, SchemaOptions.OPTIONAL),
             new SchemaNodeScalar("Count", TagType.TAG_BYTE),
             new SchemaNodeScalar("Slot", TagType.TAG_BYTE, SchemaOptions.OPTIONAL),
             new SchemaNodeCompound("tag", new SchemaNodeCompound("") {
@@ -114,7 +114,7 @@ namespace Substrate
         /// </summary>
         /// <remarks>The damage value may represent a generic data value for some items.</remarks>
         [TagNode(TagType = TagType.TAG_SHORT)]
-        public int Damage { get; set; }
+        public int? Damage { get; set; }
 
         /// <summary>
         /// Gets or sets the number of this item stacked together in an item slot.
@@ -206,7 +206,8 @@ namespace Substrate
             }
 
             Count = ctree["Count"].ToTagByte();
-            Damage = ctree["Damage"].ToTagShort();
+            Damage = ctree["Damage"]?.ToTagShort();
+            Slot = ctree["Slot"]?.ToTagByte();
 
             if (ctree.ContainsKey("tag"))
             {
