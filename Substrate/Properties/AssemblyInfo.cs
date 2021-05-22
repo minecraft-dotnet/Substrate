979022a2660046f4d87aa0cad9dd73c627ca68a4
Henry Gabryjelski <henrygab@users.noreply.github.com>
2021-05-22 12:45:25 -0700

    remove legacy info

--- Substrate/Properties/AssemblyInfo.cs
+++ Substrate/Properties/AssemblyInfo.cs
@@ -3,8 +3,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// See https://elanderson.net/2017/06/duplicate-system-reflection-assemblycompanyattribute-attribute/
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
